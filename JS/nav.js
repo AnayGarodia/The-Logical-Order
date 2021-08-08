ee8a8dd36072ef129c77ee0c368e9818575a38a1
Anay Garodia <agarodia98@gmail.com>
2021-08-08 19:59:58 +0530

    animaion

--- JS/nav.js
+++ JS/nav.js
@@ -1,19 +1,3 @@
-var $blip = $('.blip');
-
-$('#page-nav>a').on('mouseover', function() {
-    $blip.css({
-        left: $(this).offset().left -
-            $(this).parent().offset().left +
-            $(this).width() / 2 -
-            20 // 20 is one half of .blip's width
-    });
-});
-
-$('#page-nav').on('mouseout', function() {
-    $('.blip').css({ left: -100 });
-});
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.querySelector(".menu-btn");
     const navContent = document.querySelector(".nav-collapse-content")
