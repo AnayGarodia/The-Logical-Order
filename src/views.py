3881cc519336393b26098ce5a06f933c0f88c4b2
deveshkedia <dkediaphone@icloud.com>
2021-08-27 21:31:52 +0530

    aareghseugfa

--- src/views.py
+++ src/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from django.shortcuts import render
-#import tensorflow as tf
+import tensorflow as tf
+# import kerass
 import smtplib
 import ssl
+import base64
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import numpy as np
 from .models import Files
-from .forms import UploadFileForm, NewUserForm
+from .forms import UploadFileForm
 import os
 from datetime import date
 from email.mime.text import MIMEText
@@ -109,3 +111,65 @@ def sendFeedback(request):
                 sender_email, receiver_email, message.as_string()
             )
         return render(request, 'responseRecorded.html')
+
+
+def post(request):
+    if request.POST['uploadedFile'] == '':
+        form = UploadFileForm(request.POST, request.FILES)
+        print(form)
+        if form.is_valid():
+            print("ISALID")
+            files = form.cleaned_data['image']
+            fi = request.FILES['image'].name
+            today = date.today()
+            fileND = fi.split('.', 1)
+            imageFile = Files(image=files,
+                              filename=f"{fi}", date=today.strftime("%Y-%m-%d"))
+            imageFile.save()
+            filename = f"/Users/deveshkedia/Desktop/Projects/Relativity/Relativity/MEDIABIN/media/{fi}"
+            filenamesabc = f'{fi}.png'
+            fileND = filenamesabc.split('.', 1)
+            model = tf.keras.models.load_model(
+                "/Users/deveshkedia/Desktop/Projects/AIIJC/WEB/The-Logical-Order/src/Model")
+            print(filename)
+            predictions = model.predict(filename)
+            print(np.where(predictions[0] == max(predictions[0])))
+            result = np.where(predictions[0] == max(predictions[0]))
+            report = result[0][0]
+            fileND = filenamesabc.split('.', 1)
+            os.remove(
+                f"/Users/deveshkedia/Desktop/Projects/Relativity/Relativity/MEDIABIN/media/{fi}")
+            return render(request, "scan.html", {"model": report, 'file': fileND[0]})
+        else:
+            form = UploadFileForm()
+            return render(request, 'upload.html', {'form': form})
+    else:
+        raw_data = request.POST['uploadedFile']
+        img_data = raw_data.split(',', 1)
+        # print(img_data[1])
+        # or, more concisely using with statement
+        import base64
+        imgdata = base64.b64decode(img_data[1])
+        fi = img_data[0]
+        with open(fi, 'wb') as f:
+            f.write(
+                "/Users/deveshkedia/Desktop/Projects/AIIJC/WEB/The-Logical-Order/MEDIABIN/media/", imgdata)
+        filename = f"/Users/deveshkedia/Desktop/Projects/Relativity/Relativity/MEDIABIN/media/{fi}"
+        filenamesabc = fi
+        fileND = filenamesabc.split('.', 1)
+        model = tf.keras.models.load_model(
+            "/Users/deveshkedia/Desktop/Projects/AIIJC/WEB/The-Logical-Order/src/Model")
+        print(filename)
+        predictions = model.predict(filename)
+        print(np.where(predictions[0] == max(predictions[0])))
+        result = np.where(predictions[0] == max(predictions[0]))
+        report = result[0][0]
+        fileND = filenamesabc.split('.', 1)
+        os.remove(
+            f"/Users/deveshkedia/Desktop/Projects/Relativity/Relativity/MEDIABIN/media/{fi}")
+        return render(request, "scan.html", {"model": report, 'file': fileND[0]})
+    return render(request, 'hello.htnl')
+
+
+def home(request):
+    return render(request, 'landing_page_without_animation.html')
