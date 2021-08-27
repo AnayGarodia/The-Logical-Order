0b3af99a0501db28c41b69faeb18358a32d0fa68
Anay Garodia <agarodia98@gmail.com>
2021-08-27 10:15:03 +0530

    dfgy

--- src/static/JS/upload.js
+++ src/static/JS/upload.js
@@ -1,16 +1,49 @@
-dropContainer.ondragover = dropContainer.ondragenter = function (evt) {
-  evt.preventDefault();
-};
+const dropArea = document.querySelector("#dropContainer");
+let file;
+let input = document.querySelector("#file");
 
-dropContainer.ondrop = function (evt) {
-  // pretty simple -- but not for IE :(
-  fileInput.files = evt.dataTransfer.files;
+dropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    dropArea.classList.add('active');
+})
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove('active');
+})
+dropArea.addEventListener("drop", (event) => {
+    console.log(input.value)
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    let fileType = file.type;
+    let valid = ["image/jpeg", "image/png", "image/jpeg"];
+    if (valid.includes(fileType)) {
+        console.log(1);
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            let fileURL = fileReader.result;
+            fileURL = dataURLtoFile(fileURL, file);
+            console.log(fileURL)
+            input.value = fileURL;
 
-  // If you want to use some of the dropped files
-  const dT = new DataTransfer();
-  dT.items.add(evt.dataTransfer.files[0]);
-  dT.items.add(evt.dataTransfer.files[3]);
-  fileInput.files = dT.files;
+            console.log(input.value)
+        }
+        fileReader.readAsDataURL(file);
+    } else {
+        console.log(0);
+        dropArea.classList.remove('active');
+    }
+})
 
-  evt.preventDefault();
-};
+function dataURLtoFile(dataurl, filename) {
+
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+}
